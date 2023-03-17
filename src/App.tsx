5d6792e8ff9014a8c32346cbbccd0c565b61e313
Pavel Zhelukevich <pashazhelukevich1@gmail.com>
2023-03-17 21:53:57 +0300

    add eaLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React from 'react';
 
 
 function App() {
   return (
-    <div>App</div>
+    <div
+      className="q">
+      App
+    </div>
   );
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
 const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === 'development';
 const IS_PROD = NODE_ENV === 'production';
 
+function setupDevtool() {
+  if (IS_DEV) {
+    return 'eval';
+  }
+
+  if (IS_PROD) {
+    return false;
+  }
+}
+
 module.exports = {
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
   },
-  mode: 'development',
+  mode: NODE_ENV ? NODE_ENV : 'development',
   entry: path.resolve(__dirname, 'src/index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'js/main.js'
+    filename: 'js/.main.js',
   },
   module: {
     rules: [
@@ -22,14 +34,22 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
 
   plugins: [
-    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public/index.html') })
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public/index.html') }),
   ],
 
-}
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: IS_DEV,
+  },
+
+  devtool: setupDevtool(),
+};
 
